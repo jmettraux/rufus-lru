814b0653c496e60eccc1275e129173ab88d72506
John Mettraux <jmettraux@gmail.com>
2008-08-10 22:54:45 +0900

    indent=2

--- Rakefile
+++ Rakefile
@@ -16,27 +16,27 @@ RUFUS_LRU_VERSION = "1.0.2"
 
 spec = Gem::Specification.new do |s|
 
-    s.name              = "rufus-lru"
-    s.version           = RUFUS_LRU_VERSION
-    s.authors           = [ "John Mettraux" ]
-    s.email             = "jmettraux@gmail.com"
-    s.homepage          = "http://rufus.rubyforge.org/rufus-lru"
-    s.platform          = Gem::Platform::RUBY
-    s.summary           = "LruHash class, a Hash with a max size, controlled by a LRU mechanism"
-    #s.license           = "MIT"
+  s.name        = "rufus-lru"
+  s.version       = RUFUS_LRU_VERSION
+  s.authors       = [ "John Mettraux" ]
+  s.email       = "jmettraux@gmail.com"
+  s.homepage      = "http://rufus.rubyforge.org/rufus-lru"
+  s.platform      = Gem::Platform::RUBY
+  s.summary       = "LruHash class, a Hash with a max size, controlled by a LRU mechanism"
+  #s.license       = "MIT"
 
-    s.require_path      = "lib"
-    #s.autorequire       = "rufus-verbs"
-    s.test_file         = "test/test.rb"
-    s.has_rdoc          = true
-    s.extra_rdoc_files  = [ 'README.txt' ]
-    #s.requirements      << 'libxml-ruby'
-    #s.add_dependency('libxml-ruby')
+  s.require_path    = "lib"
+  #s.autorequire     = "rufus-verbs"
+  s.test_file     = "test/test.rb"
+  s.has_rdoc      = true
+  s.extra_rdoc_files  = [ 'README.txt' ]
+  #s.requirements    << 'libxml-ruby'
+  #s.add_dependency('libxml-ruby')
 
-    files = FileList[ "{bin,docs,lib,test}/**/*" ]
-    files.exclude "rdoc" 
-    files.exclude "extras" 
-    s.files = files.to_a
+  files = FileList[ "{bin,docs,lib,test}/**/*" ]
+  files.exclude "rdoc" 
+  files.exclude "extras" 
+  s.files = files.to_a
 end
 
 #
@@ -51,32 +51,32 @@ task :default => [ :clean, :repackage ]
 # TESTING
 
 Rake::TestTask.new(:test) do |t|
-    t.libs << "test"
-    t.test_files = FileList['test/test.rb']
-    t.verbose = true
+  t.libs << "test"
+  t.test_files = FileList['test/test.rb']
+  t.verbose = true
 end
 
 #
 # PACKAGING
 
 Rake::GemPackageTask.new(spec) do |pkg|
-    #pkg.need_tar = true
+  #pkg.need_tar = true
 end
 
 Rake::PackageTask.new("rufus-lru", RUFUS_LRU_VERSION) do |pkg|
-    pkg.need_zip = true
-    pkg.package_files = FileList[
-        "Rakefile",
-        "*.txt",
-        "lib/**/*",
-        "test/**/*"
-    ].to_a
-    #pkg.package_files.delete("MISC.txt")
-    class << pkg
-        def package_name
-            "#{@name}-#{@version}-src"
-        end
+  pkg.need_zip = true
+  pkg.package_files = FileList[
+    "Rakefile",
+    "*.txt",
+    "lib/**/*",
+    "test/**/*"
+  ].to_a
+  #pkg.package_files.delete("MISC.txt")
+  class << pkg
+    def package_name
+      "#{@name}-#{@version}-src"
     end
+  end
 end
 
 
@@ -88,19 +88,19 @@ ALLISON="/Library/Ruby/Gems/1.8/gems/allison-2.0.3/lib/allison.rb"
 
 Rake::RDocTask.new do |rd|
 
-    rd.main = "README.txt"
+  rd.main = "README.txt"
 
-    rd.rdoc_dir = "html/rufus-lru"
+  rd.rdoc_dir = "html/rufus-lru"
 
-    rd.rdoc_files.include(
-        "README.txt", "CHANGELOG.txt", "LICENSE.txt", "lib/**/*.rb")
+  rd.rdoc_files.include(
+    "README.txt", "CHANGELOG.txt", "LICENSE.txt", "lib/**/*.rb")
 
-    rd.title = "rufus-lru rdoc"
+  rd.title = "rufus-lru rdoc"
 
-    rd.options << '-N' # line numbers
-    rd.options << '-S' # inline source
+  rd.options << '-N' # line numbers
+  rd.options << '-S' # inline source
 
-    rd.template = ALLISON if File.exist?(ALLISON)
+  rd.template = ALLISON if File.exist?(ALLISON)
 end
 
 
@@ -109,9 +109,9 @@ end
 
 task :upload_website => [ :clean, :rdoc ] do
 
-    account = "jmettraux@rubyforge.org"
-    webdir = "/var/www/gforge-projects/rufus"
+  account = "jmettraux@rubyforge.org"
+  webdir = "/var/www/gforge-projects/rufus"
 
-    sh "rsync -azv -e ssh html/rufus-lru #{account}:#{webdir}/"
+  sh "rsync -azv -e ssh html/rufus-lru #{account}:#{webdir}/"
 end
 
--- lib/rufus/lru.rb
+++ lib/rufus/lru.rb
@@ -8,10 +8,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,118 +36,118 @@
 # least recently used entries (LRU hence), will be discared to make
 # room for the new entries.
 #
-#     require 'rubygems'
-#     require 'rufus/lru'
+#   require 'rubygems'
+#   require 'rufus/lru'
 #
-#     h = LruHash.new 3
+#   h = LruHash.new 3
 #
-#     5.times { |i| h[i] = "a" * i }
+#   5.times { |i| h[i] = "a" * i }
 #
-#     puts h.inspect # >> {2=>"aa", 3=>"aaa", 4=>"aaaa"}
+#   puts h.inspect # >> {2=>"aa", 3=>"aaa", 4=>"aaaa"}
 #
-#     h[:newer] = "b"
+#   h[:newer] = "b"
 #
-#     puts h.inspect # >> {:newer=>"b", 3=>"aaa", 4=>"aaaa"}
-# 
+#   puts h.inspect # >> {:newer=>"b", 3=>"aaa", 4=>"aaaa"}
+#
 #
 class LruHash < Hash
 
-    #--
-    #include MonitorMixin
-        #
-        # seems not necessary for now, and it collides with expool's
-        # @monitors own sync
-    #++
+  #--
+  #include MonitorMixin
+    #
+    # seems not necessary for now, and it collides with expool's
+    # @monitors own sync
+  #++
 
-    attr_reader :maxsize
+  attr_reader :maxsize
+
+  #
+  # Initializes a LruHash with a given maxsize.
+  #
+  def initialize (maxsize)
+
+    super()
+
+    @maxsize = maxsize
+    @lru_keys = []
+  end
+
+  def maxsize= (s)
+
+    @maxsize = s
+    remove_lru
+  end
+
+  def clear
+
+    super
+    @lru_keys.clear
+  end
+
+  #
+  # Returns the keys with the lru in front.
+  #
+  def ordered_keys
+
+    @lru_keys
+  end
+
+  def [] (key)
+
+    value = super
+    return nil unless value
+    touch key
+
+    value
+  end
+
+  def []= (key, value)
+
+    remove_lru
+    super
+    touch key
+
+    value
+  end
+
+  def merge! (hash)
+
+    hash.each { |k, v| self[k] = v }
+
+    # not using 'super', but in order not guaranteed at all...
+  end
+
+  def delete (key)
+
+    value = super
+    @lru_keys.delete key
+
+    value
+  end
+
+  protected
 
     #
-    # Initializes a LruHash with a given maxsize.
+    # Puts the key on top of the lru 'stack'.
+    # The bottom being the lru place.
     #
-    def initialize (maxsize)
+    def touch (key)
 
-        super()
-
-        @maxsize = maxsize
-        @lru_keys = []
-    end
-
-    def maxsize= (s)
-
-        @maxsize = s
-        remove_lru
-    end
-
-    def clear
-
-        super
-        @lru_keys.clear
+      @lru_keys.delete key
+      @lru_keys << key
     end
 
     #
-    # Returns the keys with the lru in front.
+    # Makes sure that the hash fits its maxsize. If not, will remove
+    # the least recently used items.
     #
-    def ordered_keys
+    def remove_lru
 
-        @lru_keys
+      while size >= @maxsize
+
+        key = @lru_keys.delete_at 0
+        delete key
+      end
     end
-
-    def [] (key)
-
-        value = super
-        return nil unless value
-        touch key
-
-        value
-    end
-
-    def []= (key, value)
-
-        remove_lru
-        super
-        touch key
-
-        value
-    end
-
-    def merge! (hash)
-
-        hash.each { |k, v| self[k] = v }
-
-        # not using 'super', but in order not guaranteed at all...
-    end
-
-    def delete (key)
-
-        value = super
-        @lru_keys.delete key
-
-        value
-    end
-
-    protected
-
-        #
-        # Puts the key on top of the lru 'stack'.
-        # The bottom being the lru place.
-        #
-        def touch (key)
-
-            @lru_keys.delete key
-            @lru_keys << key
-        end
-
-        #
-        # Makes sure that the hash fits its maxsize. If not, will remove 
-        # the least recently used items.
-        #
-        def remove_lru
-
-            while size >= @maxsize
-
-                key = @lru_keys.delete_at 0
-                delete key
-            end
-        end
 end
 
