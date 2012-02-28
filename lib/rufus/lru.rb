94b97b3646f474cc6a76567541d9dcab5e7df1b8
John Mettraux <jmettraux@gmail.com>
2012-02-28 13:23:41 +0900

    2012

--- lib/rufus/lru.rb
+++ lib/rufus/lru.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2007-2010, John Mettraux, jmettraux@gmail.com
+# Copyright (c) 2007-2012, John Mettraux, jmettraux@gmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,7 @@
 
 module Rufus
 module Lru
+
   VERSION = '1.0.3'
 end
 end
